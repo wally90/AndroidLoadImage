d7ae87fde4cfba11bb518a0cdd6dea75da2bbd7b
wally90 <maskal412@mail.ru>
2015-10-06 22:52:39 +0300

    project1

---
+++ app/src/main/java/androidloadimage/org/android/androidloadimage/MainActivity.java
@@ -0,0 +1,74 @@
+package androidloadimage.org.android.androidloadimage;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.net.URL;
+
+public class MainActivity extends Activity {
+    Button load_img;
+    ImageView img;
+    Bitmap bitmap;
+    ProgressDialog pDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        load_img = (Button)findViewById(R.id.load);
+        img = (ImageView)findViewById(R.id.img);
+        load_img.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                new LoadImage().execute("http://go.mail.ru/search_images?fm=1&q=%D1%84%D0%BE%D1%82%D0%BE&frm=web#urlhash=6723824898889537859");
+            }
+        });
+
+
+    }
+    private class LoadImage extends AsyncTask<String, String, Bitmap> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Loading Image ....");
+            pDialog.show();
+
+        }
+        protected Bitmap doInBackground(String... args) {
+            try {
+                bitmap = BitmapFactory.decodeStream((InputStream)new URL(args[0]).getContent());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        protected void onPostExecute(Bitmap image) {
+
+            if(image != null){
+                img.setImageBitmap(image);
+                pDialog.dismiss();
+
+            }else{
+
+                pDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Image Does Not exist or Network Error", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+}
